37502a32dd072887abb41c1937a69a151487435e
rtd <rtd>
2023-09-16 21:57:27 +0800

    modify: init

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,41 +1,193 @@
-import { type StateUpdater, useState } from "preact/hooks";
+import { type StateUpdater, useState, useEffect, useRef } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
+// import { Peer } from "peerjs";
 
+const Peer = window.Peer as unknown as any;
 interface CounterProps {
   start: number;
 }
 
-async function fetchSetCount(newCount: number) {
-  return await fetch(`/api/count?new_count=${newCount}`, { method: "POST" });
+const download = (data, filename, mime, bom) => {
+  var blobData = typeof bom !== "undefined" ? [bom, data] : [data];
+  var blob = new Blob(blobData, { type: mime || "application/octet-stream" });
+  if (typeof window.navigator.msSaveBlob !== "undefined") {
+    // IE workaround for "HTML7007: One or more blob URLs were
+    // revoked by closing the blob for which they were created.
+    // These URLs will no longer resolve as the data backing
+    // the URL has been freed."
+    window.navigator.msSaveBlob(blob, filename);
+  } else {
+    var blobURL =
+      window.URL && window.URL.createObjectURL
+        ? window.URL.createObjectURL(blob)
+        : window.webkitURL.createObjectURL(blob);
+    var tempLink = document.createElement("a");
+    tempLink.style.display = "none";
+    tempLink.href = blobURL;
+    tempLink.setAttribute("download", filename);
+    if (typeof tempLink.download === "undefined") {
+      tempLink.setAttribute("target", "_blank");
+    }
+
+    document.body.appendChild(tempLink);
+    tempLink.click();
+
+    setTimeout(function () {
+      document.body.removeChild(tempLink);
+      window.URL.revokeObjectURL(blobURL);
+    }, 200);
+  }
+};
+
+function fileToBlob(file) {
+  // 创建 FileReader 对象
+  let reader = new FileReader();
+  return new Promise((resolve) => {
+    // FileReader 添加 load 事件
+    reader.addEventListener("load", (e) => {
+      let blob;
+      if (typeof e.target.result === "object") {
+        blob = new Blob([e.target.result]);
+      } else {
+        blob = e.target.result;
+      }
+      console.log(Object.prototype.toString.call(blob));
+      resolve(blob);
+    });
+    // FileReader 以 ArrayBuffer 格式 读取 File 对象中数据
+    reader.readAsArrayBuffer(file);
+  });
 }
 
-export default function Counter(props: CounterProps) {
-  const [count, setCount] = useState(props.start);
+export default function Counter() {
+  const [peers, setPeers] = useState([]);
+  const [user, setUser] = useState(localStorage.getItem("user"));
+  const peerRef = useRef();
+  const socketRef = useRef();
+
+  useEffect(() => {
+    if (!!user) {
+      peerRef.current = new Peer();
+      handleIncomingConnection();
+      startWebsocket();
+      window.addEventListener("beforeunload", disconnectWebsocket);
+    }
+  }, [user]);
+
+  const startWebsocket = () => {
+    const socket = new WebSocket("ws://localhost:8000");
+    socketRef.current = socket;
+    socket.addEventListener("open", function (event) {
+      peerRef.current.id &&
+        socket.send(
+          JSON.stringify({
+            type: "add",
+            data: {
+              user: user,
+              peerId: peerRef.current.id,
+            },
+          })
+        );
+    });
+
+    // Listen for messages
+    socket.addEventListener("message", function (event) {
+      console.log(event.data);
+      setPeers(JSON.parse(event.data));
+    });
+  };
+
+  const disconnectWebsocket = () => {
+    socketRef.current?.close?.();
+  };
+
+  const handleIncomingConnection = () => {
+    peerRef.current?.on("connection", function (conn) {
+      console.log("Incoming connection: " + conn);
+      conn.on("open", function () {
+        console.log("open: " + conn.id);
+        conn.on("data", function (receivedData) {
+          console.log("Receiving data");
+          let data = receivedData;
+          download(data.file || "", data.fileName || "fileName", data.fileType);
+        });
+      });
+    });
+  };
+
+  const connectPeer = (id) => {
+    return new Promise((resolve, reject) => {
+      try {
+        let conn = peerRef.current.connect(id, { reliable: true });
+        if (!conn) {
+          reject(new Error("Connection can't be established"));
+        } else {
+          conn
+            .on("open", function () {
+              console.log("Connect to: " + id);
+              resolve({ id, conn });
+            })
+            .on("error", function (err) {
+              console.log(err);
+              reject(err);
+            });
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  };
+
+  const handleChange = async (id, e) => {
+    const fileList = e.target.files;
+    const { conn } = await connectPeer(id);
+    let file = fileList[0] as unknown as File;
+    let blob = new Blob([file], { type: file.type });
+    blob = await blob.arrayBuffer();
+
+    conn.send({
+      dataType: "FILE",
+      file: blob,
+      fileName: file.name,
+      fileType: file.type,
+    });
+  };
+
+  const handleUserChange = (e) => {
+    localStorage.setItem("user", e.target.value);
+    setUser(e.target.value);
+  };
+
   return (
-    <div class="flex gap-2 w-full">
-      <p class="flex-grow-1 font-bold text-xl">{count}</p>
-      <Button
-        onClick={async () => {
-          const newCount = count - 1;
-          const resp = await fetchSetCount(newCount);
-          if (resp.ok) {
-            setCount(newCount);
-          }
-        }}
-      >
-        -1
-      </Button>
-      <Button
-        onClick={async () => {
-          const newCount = count + 1;
-          const resp = await fetchSetCount(newCount);
-          if (resp.ok) {
-            setCount(newCount);
-          }
-        }}
-      >
-        +2
-      </Button>
+    <div>
+      {!user ? (
+        <input
+          className={"bg-blue-200 active:border-black"}
+          onChange={handleUserChange}
+        />
+      ) : (
+        <div className="font-bold pb-4">{user}</div>
+      )}
+
+      <div className={"w-full flex gap-2 flex-col"}>
+        {peers?.map((i) => (
+          <div className={"w-full"}>
+            <label
+              className="w-full transition-all cursor-pointer p-2 inline-block bg-blue-100 hover:bg-blue-200"
+              for={i.peerId}
+            >
+              {i.user}
+            </label>
+            <input
+              className="hidden"
+              onChange={handleChange.bind(null, i.peerId)}
+              id={i.peerId}
+              type="file"
+              class="bg-blue-200"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,41 @@
 import type { Handlers, PageProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
-import { getCount } from "../utils/db.ts";
 
-interface HomeProps {
-  start: number;
-}
+interface HomeProps {}
+
+let peers = new Map();
+let sockets = [];
 
 export const handler: Handlers<HomeProps> = {
-  async GET(_req, ctx) {
-    let start = await getCount();
-    if (start === null) start = 3;
-    return ctx.render({ start });
+  async GET(req, ctx) {
+    if (req.headers.get("upgrade") != "websocket") {
+      return ctx.render();
+    }
+
+    const { socket, response } = Deno.upgradeWebSocket(req);
+    sockets.push(socket);
+    socket.addEventListener("open", () => {
+      socket?.send(JSON.stringify([...peers.values()]));
+    });
+
+    socket.addEventListener("close", () => {
+      sockets = sockets.filter((i) => i !== socket);
+    });
+
+    socket.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data);
+      console.log(socket.OPEN);
+      if (data.type === "add") {
+        peers.set(data.data.user, data.data);
+        sockets.forEach((socket) => {
+          if (socket.readyState === socket.OPEN) {
+            socket?.send(JSON.stringify([...peers.values()]));
+          }
+        });
+      }
+    });
+    return response;
   },
 };
 
@@ -19,19 +43,12 @@ export default function Home(props: PageProps<HomeProps>) {
   return (
     <>
       <Head>
+        <meta lang="zh-CN"/>
         <title>Fresh App with Deno KV</title>
+        <script src="https://unpkg.com/peerjs@1.5.0/dist/peerjs.js"></script>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={props.data.start} />
+      <div class="p-4">
+        <Counter />
       </div>
     </>
   );
